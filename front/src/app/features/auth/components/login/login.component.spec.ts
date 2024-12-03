ed114092472c00fe15fc94552115aa5039094872
ultimecreation <naserbechata@gmail.com>
2024-12-03 17:15:38 +0100

    auth tests + added phl library

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,32 +11,96 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { LoginRequest } from '../../interfaces/loginRequest.interface';
+import { of, throwError } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { phl } from '@angular-extensions/pretty-html-log';
+
+const validLoginFormData: LoginRequest = {
+    email: "user@mail.com",
+    password: "password"
+}
+const invalidLoginFormData: LoginRequest = {
+    email: "",
+    password: ""
+}
+
+const responseForValidCredentials = {
+    "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5b2dhQHN0dWRpby5jb20iLCJpYXQiOjE3MzMyMzgzNDgsImV4cCI6MTczMzMyNDc0OH0.UPFvmM09njhH6AJ0zGA1-IWlYR2qOsxaQyrGVTIDPyyzoN99bCD1gTtofLOD_fmk11lf4h-Grwht4JkZTJ42HA",
+    "type": "Bearer",
+    "id": 1,
+    "username": "yoga@studio.com",
+    "firstName": "Admin",
+    "lastName": "Admin",
+    "admin": true
+}
+
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
+    let mockedAuthService: any
+    let mockedRouter: any
+    let mockedSessionService: any
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LoginComponent],
-      providers: [SessionService],
-      imports: [
-        RouterTestingModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        MatCardModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatInputModule,
-        ReactiveFormsModule]
+
+    beforeEach(async () => {
+
+        mockedAuthService = {
+            login: jest.fn().mockReturnValue({ subscribe: jest.fn })
+        }
+        mockedRouter = {
+            navigate: jest.fn()
+        }
+        mockedSessionService = {
+            logIn: jest.fn()
+        }
+        await TestBed.configureTestingModule({
+            declarations: [LoginComponent],
+            providers: [
+                { provide: AuthService, useValue: mockedAuthService },
+                { provide: Router, useValue: mockedRouter },
+                { provide: SessionService, useValue: mockedSessionService },
+            ],
+            imports: [
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                HttpClientModule,
+                MatCardModule,
+                MatIconModule,
+                MatFormFieldModule,
+                MatInputModule,
+                ReactiveFormsModule]
+        })
+            .compileComponents();
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Should login the user when valid credentials are sent', () => {
+
+        mockedAuthService.login.mockReturnValue(of(responseForValidCredentials))
+        component.form.setValue(validLoginFormData)
+        component.submit()
+
+        expect(mockedAuthService.login).toHaveBeenCalledWith(validLoginFormData)
+        expect(mockedSessionService.logIn).toHaveBeenCalledWith(responseForValidCredentials)
+        expect(mockedRouter.navigate).toHaveBeenCalledWith(["/sessions"])
     })
-      .compileComponents();
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('Should not login the user when invalid credentials are sent', () => {
+        const errorMsg = "An error occurred"
+        mockedAuthService.login.mockReturnValue(throwError(() => new Error(errorMsg)))
+        component.form.setValue(invalidLoginFormData)
+        component.submit()
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        expect(mockedSessionService.logIn).not.toHaveBeenCalled()
+        expect(mockedRouter.navigate).not.toHaveBeenCalledWith(["/sessions"])
+        expect(errorMsg).toBeDefined()
+    })
 });
--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -9,32 +9,91 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import { AuthService } from '../../services/auth.service';
+import { of, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { phl } from '@angular-extensions/pretty-html-log';
+import { RegisterRequest } from '../../interfaces/registerRequest.interface';
+
+
+const validRegisterFormData: RegisterRequest = {
+    email: "user@mail.com",
+    firstName: "firstname",
+    lastName: "lastname",
+    password: "password"
+}
+const invalidRegisterFormData: RegisterRequest = {
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: ""
+}
+
+
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+    let component: RegisterComponent;
+    let fixture: ComponentFixture<RegisterComponent>;
+    let mockedAuthService: any
+    let mockedRouter: any
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [RegisterComponent],
-      imports: [
-        BrowserAnimationsModule,
-        HttpClientModule,
-        ReactiveFormsModule,  
-        MatCardModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule
-      ]
+
+    beforeEach(async () => {
+        mockedAuthService = {
+            register: jest.fn().mockReturnValue({ subscribe: jest.fn })
+        }
+        mockedRouter = {
+            navigate: jest.fn()
+        }
+
+        await TestBed.configureTestingModule({
+            declarations: [RegisterComponent],
+            providers: [
+                { provide: AuthService, useValue: mockedAuthService },
+                { provide: Router, useValue: mockedRouter },
+            ],
+            imports: [
+                BrowserAnimationsModule,
+                HttpClientModule,
+                ReactiveFormsModule,
+                MatCardModule,
+                MatFormFieldModule,
+                MatIconModule,
+                MatInputModule
+            ]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(RegisterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Should register a new user when all fields are valid', () => {
+
+        mockedAuthService.register.mockReturnValue(of(validRegisterFormData))
+        component.form.setValue(validRegisterFormData);
+        component.submit();
+
+        expect(mockedAuthService.register).toHaveBeenCalledWith(validRegisterFormData);
+        expect(mockedRouter.navigate).toHaveBeenCalledWith(["/login"])
+
     })
-      .compileComponents();
+    it('Should not register a new user when some or all fields are empty', () => {
 
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        const errorMsg = "An error occurred"
+        mockedAuthService.register.mockReturnValue(throwError(() => new Error(errorMsg)))
+        component.form.setValue(invalidRegisterFormData);
+        component.submit();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        expect(mockedRouter.navigate).not.toHaveBeenCalledWith(["/login"])
+        expect(errorMsg).toBeDefined();
+
+
+    })
 });
